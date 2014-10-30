cefff4ecf1ec60f53f1498f3fc67e4da01488611
santi <santi.grau.torredemer@gmail.comgit>
2014-10-30 16:31:10 +0100

    added converter

---
+++ public/js/controllers/app.js
@@ -0,0 +1,54 @@
+define(['angularAMD'], function (angularAMD) {
+	var App = angular.module('uploader', []).controller('dropFile', ['$scope', function($scope) {
+		$scope.handleDrop = function(e) {
+			(e && e.preventDefault) && e.preventDefault();
+			$scope.$apply(function(){ $scope.state = 'converting'; })
+			var dt = e.dataTransfer;
+			var files = dt.files;
+			for (var i=0; i < files.length; i++) {
+				var reader = new FileReader();
+				reader.addEventListener('loadend', $scope.fileLoadEnd, false);
+				reader.readAsArrayBuffer(files[i]);
+			}
+			$scope.$apply(function(){ $scope.state = 'loading'; });
+		};
+		$scope.handleDragOver = function(e) {
+			(e && e.preventDefault) && e.preventDefault();
+			$scope.$apply(function(){ $scope.state = 'dragging'; })
+		};
+		$scope.handleDragLeave = function(e) {
+			(e && e.preventDefault) && e.preventDefault();
+			$scope.$apply(function(){ $scope.state = ''; })
+		};
+		$scope.fileLoadEnd = function(data){
+			console.log(data.target)
+			var xmlhttp=new XMLHttpRequest();
+			xmlhttp.open("POST","http://ec2-54-69-52-7.us-west-2.compute.amazonaws.com/data");
+			xmlhttp.send(data.target.result);
+			xmlhttp.onreadystatechange=function() {
+				if (xmlhttp.readyState==4 && xmlhttp.status==200){
+					console.log('data received----------')
+					console.log(xmlhttp.responseText);
+					console.log('font----------')
+					var data = JSON.parse( xmlhttp.responseText );
+					console.log(data)
+					setTimeout(function(){
+						// $scope.$apply(function(){ $scope.state = ''; });
+					}, 2000)
+				}
+			}
+		}
+		$scope.state = '';
+	}]);
+	App.directive('droppable', function () {
+		return {
+			restrict: 'A',
+			link: function (scope, element, attrs) {
+				element[0].addEventListener('drop', scope.handleDrop, false);
+				element[0].addEventListener('dragover', scope.handleDragOver, false);
+				element[0].addEventListener('dragleave', scope.handleDragLeave, false);
+			}
+		}
+	});
+	return angularAMD.bootstrap(App);
+});
