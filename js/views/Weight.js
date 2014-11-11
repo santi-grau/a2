8244aa6c7e51fff3c27572871cc300048e1c2783
santi <santi.grau.torredemer@gmail.com>
2014-11-11 10:51:05 +0100

    added hash

--- js/views/Weight.js
+++ js/views/Weight.js
@@ -37,8 +37,15 @@ define(['backbone'],
 				if(this.typingInterval) clearTimeout(this.typingInterval);
 				this.typingInterval = setTimeout(_.bind(this.saveName, this), 1000);
 			},
+			stripVowelAccent: function(str){
+				var rExps=[ {re:/[\xC0-\xC6]/g, ch:'A'}, {re:/[\xE0-\xE6]/g, ch:'a'}, {re:/[\xC8-\xCB]/g, ch:'E'}, {re:/[\xE8-\xEB]/g, ch:'e'}, {re:/[\xCC-\xCF]/g, ch:'I'}, {re:/[\xEC-\xEF]/g, ch:'i'}, {re:/[\xD2-\xD6]/g, ch:'O'}, {re:/[\xF2-\xF6]/g, ch:'o'}, {re:/[\xD9-\xDC]/g, ch:'U'}, {re:/[\xF9-\xFC]/g, ch:'u'}, {re:/[\xD1]/g, ch:'N'}, {re:/[\xF1]/g, ch:'n'}, {re:/\s+/g, ch:'-'} ];
+				for(var i=0, len=rExps.length; i<len; i++) str = str.replace(rExps[i].re, rExps[i].ch);
+				return str;
+			},
 			saveName: function(){
 				this.model.set('name', this.$('.weightName').val());
+				this.model.set('hash', this.stripVowelAccent(this.$('.weightName').val()));
+				console.log(this.model)
 			}
 		});
 		return Weight;
